03602517c9dc13c35c15a3bd87a24bffd266933e
TROUSSELLIER Laurent <laurent.troussellier@ipsl.fr>
2024-10-23 10:31:52 +0200

    add normalized

--- src/cmipld/cv.py
+++ src/cmipld/cv.py
@@ -1,18 +1,24 @@
 from cmipld import settings
 from cmipld import model_mapping
-from pyld import jsonld
-from cmipld.core import Data
+from cmipld.data import Data
+
 class CV():
     def __init__(self):
         self.url = settings.URL_UNIVERSE
 
-    def parse_datadescriptor(self,datadescriptor):
+    def get_terms(self,datadescriptor):
         data =  Data(self.url+datadescriptor+".json")
         terms_data =  [Data(term["@id"]) for term in data.expanded[0][self.url+datadescriptor+"/terms"]]
         #print(terms_data)
         model = model_mapping[datadescriptor]
         terms = [ model(**elem.json) for elem in terms_data ] 
         return  terms
+
+    def get_term(self, datadescriptor_id, term_id):
+        data = Data(self.url+datadescriptor_id+"/"+term_id)
+        model = model_mapping[datadescriptor_id]
+        print(data.normalized)
+        return model(**data.json)
 
     def parse_datadescriptor_term(self,datadescriptor_name):
 
--- src/cmipld/data.py
+++ src/cmipld/data.py
@@ -1,3 +1,4 @@
+from typing import LiteralString
 import requests
 from pyld import jsonld
 from dataclasses import dataclass
@@ -7,30 +8,34 @@ urllib3.disable_warnings()
 @dataclass
 class Data():
     uri: str # url
-    ressource : str # last part of the uri
-    json : dict
+    json : dict | None # None if fetch doesn't get response from uri
     expanded : dict # the json expanded with context thanks to pyld
-    uri : str
+    normalized : LiteralString | dict
 
     def __init__(self,uri):
-        self.ressource = uri.split("/")[-1]
-        self.json = self.fetch(uri)
-        self.expanded = jsonld.expand(uri)   
+        
         self.uri = uri
+        self.json = self.fetch(uri)
+        self.expanded = jsonld.expand(uri)[0] 
+        #print(self.expanded)
+        self.normalized = jsonld.normalize(uri, {'algorithm': 'URDNA2015', 'format': 'application/n-quads'})
+        print(type(self.normalized))
 
     def __str__(self):
         res = "# "*50
 
-        res += "\n"+"# "*20 + f"   {self.ressource}   "+"# "*20 +"\n"
+        res += "\n"+"# "*20 + f"   {self.uri.split("/")[-1]}   "+"# "*20 +"\n"
         res += "# "*50
 
 
         res += f"\nuri : {self.uri}\n"
         res += f"\njson version :\n {self.json}"
-        res += f"\n\nexpanded verison :\n {self.expanded}"
+        res += f"\n\nexpanded version :\n {self.expanded}"
+        res += f"\n\nnormalized version :\n {self.normalized}"
+
         return res 
 
-    def fetch(self,uri):
+    def fetch(self,uri) -> dict | None:
         try:
             # Make a GET request to the API endpoint using requests.get()
             response = requests.get(uri, headers = {'accept': 'application/json'}, verify=False)
