2345e99129353432e29d7239de0ad4c7214a5d3f
Sébastien Gardoll <sebastien@gardoll.fr>
2024-11-18 16:07:12 +0100

    rename json_content into specs

--- src/cmipld/models/sqlmodel/project.py
+++ src/cmipld/models/sqlmodel/project.py
@@ -29,7 +29,7 @@ class PTerm(SQLModel, PkMixin, IdMixin, table=True):
 
 
 def create_drs_name_index():
-    PTerm.__table_args__ = (sa.Index('drs_name_index', PTerm.__table__.c.json_content['drs_name']))
+    PTerm.__table_args__ = (sa.Index('drs_name_index', PTerm.__table__.c.specs['drs_name']))
 
 
 def project_create_db():  
