838b453362d27e62303b47fa36781dd4ca7fd2f6
Sébastien Gardoll <sebastien@gardoll.fr>
2024-11-21 14:56:39 +0100

    precise exception

--- src/cmipld/utils/functions.py
+++ src/cmipld/utils/functions.py
@@ -12,4 +12,4 @@ def get_pydantic_class(data_descriptor_id: str) -> type[BaseModel]:
     if data_descriptor_id in DATA_DESCRIPTOR_CLASS_MAPPING:
         return DATA_DESCRIPTOR_CLASS_MAPPING[data_descriptor_id]
     else:
-        raise Exception(f"{data_descriptor_id} pydantic class not found")
+        raise ValueError(f'{data_descriptor_id} pydantic class not found')
