57ec6683fa37adf65fd9ede8fdf9447876365408
Sébastien Gardoll <sebastien@gardoll.fr>
2024-11-22 14:51:34 +0100

    integrate search modalities

--- src/cmipld/univers.py
+++ src/cmipld/univers.py
@@ -5,6 +5,7 @@ from sqlmodel import select, Session
 import cmipld.db as db
 import cmipld.utils.functions as functions
 from cmipld.models.sqlmodel.univers import UTerm, DataDescriptor
+from cmipld.utils.functions import SearchSettings, create_str_comparison_expression
 
 ############## DEBUG ##############
 # TODO: to be deleted.
@@ -20,11 +21,13 @@ def _get_all_data_descriptors(session: Session) -> list[DataDescriptor]:
     return result
 
 
-def _get_data_descriptor(data_descriptor_id: str, session: Session) -> DataDescriptor:
-    statement = select(DataDescriptor).where(DataDescriptor.id==data_descriptor_id)
-    results = session.exec(statement)
-    result = results.one()
-    return result
+def _get_data_descriptor(data_descriptor_id: str, settings: SearchSettings, session: Session) -> list[DataDescriptor]:
+    where_expression = create_str_comparison_expression(field=DataDescriptor.id,
+                                                        value=data_descriptor_id,
+                                                        settings=settings)
+    statement = select(DataDescriptor).where(where_expression)
+    results = session.exec(statement).all()
+    return results
 
 
 def _get_terms(data_descriptor: DataDescriptor) -> list[type[BaseModel]]:
@@ -35,29 +38,37 @@ def _get_terms(data_descriptor: DataDescriptor) -> list[type[BaseModel]]:
     return result
 
 
-def _get_term(data_descriptor_id: str, term_id: str, session: Session) -> UTerm:
+# Settings only apply on the term_id comparison.
+def _get_term(data_descriptor_id: str, term_id: str, settings: SearchSettings, session: Session) -> list[UTerm]:
+    where_expression = create_str_comparison_expression(field=UTerm.id,
+                                                        value=term_id,
+                                                        settings=settings)
     statement = select(UTerm).join(DataDescriptor).where(DataDescriptor.id==data_descriptor_id,
-                                                         UTerm.id==term_id)
-    results = session.exec(statement)
-    result = results.one()
+                                                         where_expression)
+    results = session.exec(statement).all()
+    return results
+
+
+def get_term(data_descriptor_id: str, term_id: str, settings: SearchSettings = SearchSettings()) -> dict[str: type[BaseModel]]:
+    with UNIVERS_DB_CONNECTION.create_session() as session:
+        result = dict()
+        terms = _get_term(data_descriptor_id, term_id, settings, session)
+        term_class = functions.get_pydantic_class(data_descriptor_id)
+        for term in terms:
+            result[term.id] = term_class(**term.specs)
     return result
 
 
-def get_term(data_descriptor_id: str, term_id: str) -> type[BaseModel]:
+def get_terms(data_descriptor_id: str, settings: SearchSettings = SearchSettings()) -> dict[str, dict[str, type[BaseModel]]]:
+    result = dict()
     with UNIVERS_DB_CONNECTION.create_session() as session:
-        term = _get_term(data_descriptor_id, term_id, session)
-        term_class = functions.get_pydantic_class(data_descriptor_id)
-        return term_class(**term.specs)
-
-
-def get_terms(data_descriptor_id: str) -> dict[str, type[BaseModel]]:
-    with UNIVERS_DB_CONNECTION.create_session() as session:
-        data_descriptor = _get_data_descriptor(data_descriptor_id, session)
-        terms = _get_terms(data_descriptor)
-        result = dict()
-        for term in terms:
-            result[term.id] = term
-        return result
+        data_descriptors = _get_data_descriptor(data_descriptor_id, settings, session)
+        for data_descriptor in data_descriptors:
+            result[data_descriptor.id] = dict()
+            terms = _get_terms(data_descriptor)
+            for term in terms:
+                result[data_descriptor.id][term.id] = term
+    return result
 
 
 def get_all_data_descriptors() -> dict[str, dict]:
