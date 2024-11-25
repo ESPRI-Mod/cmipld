73e5ea07c1778859eda2ec6c5a415778f107ef65
Sébastien Gardoll <sebastien@gardoll.fr>
2024-11-25 18:36:38 +0100

    update data descriptor key

--- src/cmipld/settings.py
+++ src/cmipld/settings.py
@@ -11,13 +11,14 @@ LOG_LEVEL = logging.DEBUG
 DIRNAME_AND_FILENAME_SEPARATOR = "_"
 
 ROOT_DIR_PATH = Path(os.path.abspath(__file__)).parents[2]
-SKIPED_DIRNAMES = {"_src", "_tests", ".git", ".venv", ".pytest_cache"} # DEBUG
+ # TODO: should be directories should be reworked.
+SKIPED_DIR_ITEMS = ['_', '.', 'src']
 
 PROJECT_SPECS_FILENAME = "project_specs.json"
 PROJECT_ID_JSON_KEY = "project_id"
 CONTEXT_FILENAME = "000_context.jsonld"
 CONTEXT_JSON_KEY = "@context"
-DATA_DESCRIPTOR_JSON_KEY = "@id"
+DATA_DESCRIPTOR_JSON_KEY = "@base" # DEBUG: should be replaced by the type key in term json files.
 TERM_ID_JSON_KEY = 'id'
 PATTERN_JSON_KEY = 'pattern'
 COMPOSITE_JSON_KEY = 'parts'
