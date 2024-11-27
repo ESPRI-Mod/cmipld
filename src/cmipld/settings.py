005867694f5a61006b9950477456bb8ef0643951
Sébastien Gardoll <sebastien@gardoll.fr>
2024-11-27 13:54:29 +0100

    minor reformatting

--- src/cmipld/settings.py
+++ src/cmipld/settings.py
@@ -10,15 +10,19 @@ LOG_LEVEL = logging.DEBUG
 DIRNAME_AND_FILENAME_SEPARATOR = "_"
 
 ROOT_DIR_PATH = Path(os.path.abspath(__file__)).parents[2]
-SKIPED_FILE_DIR_NAME_PREFIXES = ['_', '.', 'src']  # TODO: should be directories should be reworked.
 
 PROJECT_SPECS_FILENAME = "project_specs.json"
 PROJECT_ID_JSON_KEY = "project_id"
 CONTEXT_FILENAME = "000_context.jsonld"
 CONTEXT_JSON_KEY = "@context"
-DATA_DESCRIPTOR_JSON_KEY = "@base" # DEBUG: should be replaced by the type key in term json files.
 TERM_ID_JSON_KEY = 'id'
 PATTERN_JSON_KEY = 'pattern'
 COMPOSITE_JSON_KEY = 'parts'
 
 logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT, handlers=LOG_HANDLERS)
+
+# TODO: file names should be rework.
+SKIPED_FILE_DIR_NAME_PREFIXES = ['_', '.', 'src']
+
+# TODO: should be replaced by the type key in term json files.
+DATA_DESCRIPTOR_JSON_KEY = "@base"
