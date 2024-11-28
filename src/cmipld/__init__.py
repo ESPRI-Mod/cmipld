0c232989aa6c7da0387f47fb40f196d4ac46b319
Sébastien Gardoll <sebastien@gardoll.fr>
2024-11-28 14:51:06 +0100

    rename parameter

--- src/cmipld/__init__.py
+++ src/cmipld/__init__.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel
 from cmipld.api.data_descriptors import DATA_DESCRIPTOR_CLASS_MAPPING
 
 
-def get_pydantic_class(data_descriptor_id: str) -> type[BaseModel]:
-    if data_descriptor_id in DATA_DESCRIPTOR_CLASS_MAPPING:
-        return DATA_DESCRIPTOR_CLASS_MAPPING[data_descriptor_id]
+def get_pydantic_class(data_descriptor_id_or_term_type: str) -> type[BaseModel]:
+    if data_descriptor_id_or_term_type in DATA_DESCRIPTOR_CLASS_MAPPING:
+        return DATA_DESCRIPTOR_CLASS_MAPPING[data_descriptor_id_or_term_type]
     else:
-        raise ValueError(f"{data_descriptor_id} pydantic class not found")
+        raise ValueError(f"{data_descriptor_id_or_term_type} pydantic class not found")
