73a7d9e13e635559d72f5a9bff5e14cb448cfb9a
Sébastien Gardoll <sebastien@gardoll.fr>
2024-12-05 14:13:13 +0100

    impl get all terms in a project

--- src/cmipld/api/projects.py
+++ src/cmipld/api/projects.py
@@ -1,8 +1,11 @@
+from pydantic import BaseModel
+from cmipld import get_pydantic_class
 from sqlmodel import Session, select
 from cmipld.api import SearchSettings, create_str_comparison_expression
 
+import cmipld.settings as settings
 import cmipld.db as db
-from cmipld.db.models.project import Project
+from cmipld.db.models.project import Project, Collection
 
 
 ############## DEBUG ##############
@@ -10,6 +13,42 @@ from cmipld.db.models.project import Project
 # The following instructions are only temporary as long as a complet data managment will be implmented.
 CMIP6PLUS_DB_CONNECTION = db.DBConnection(db.CMIP6PLUS_DB_FILE_PATH, 'cmip6plus', False)
 ###################################
+
+
+def _get_all_collections_in_project(project_id: str, session: Session) -> list[Collection]:
+    statement = select(Project).where(Project.id == project_id)
+    project = session.exec(statement).one()
+    return project.collections
+
+
+def _get_all_terms_in_collection(collection: Collection) -> list[type[BaseModel]]:
+    result = list()
+    for term in collection.terms:
+        term_class = get_pydantic_class(term.specs[settings.TERM_TYPE_JSON_KEY])
+        result.append(term_class(**term.specs))
+    return result
+
+
+def get_all_terms_in_project(project_id: str) -> dict[str, dict[str, type[BaseModel]]]:
+    """
+    Gets all the terms of the given project.
+    This function performs an exact match on the `project_id` and does **not** search for similar or related projects.
+    As terms are unique within a collection but may have some synonyms within a project,
+    the result maps every term to their collection.
+
+    :returns: A dictionary that maps collection ids to a mapping of term ids and their corresponding Pydantic model instances.
+    :rtype: dict[str, dict[str, type[BaseModel]]]
+    """
+    with CMIP6PLUS_DB_CONNECTION.create_session() as session:
+        collections = _get_all_collections_in_project(project_id, session)
+        result = dict()
+        for collection in collections:
+            # Term may have some sysnonyms within a project.
+            result[collection.id] = dict()
+            terms = _get_all_terms_in_collection(collection)
+            for term in terms:
+                result[collection.id][term.id] = term
+    return result
 
 
 def _find_project(project_id: str,
@@ -53,4 +92,4 @@ def find_project(project_id: str,
 
 
 if __name__ == "__main__":
-    find_project('cmip6plus')
+    get_all_terms_in_project('cmip6plus')
