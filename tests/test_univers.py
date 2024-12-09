133b0653079d970844df32b92ef82665d37afc58
Sébastien Gardoll <sebastien@gardoll.fr>
2024-12-09 14:23:01 +0100

    feat:add settings

--- tests/test_univers.py
+++ tests/test_univers.py
@@ -1,4 +1,5 @@
 import cmipld.api.univers as univers
+from cmipld.api import SearchSettings, SearchType
 
 _SOME_DATA_DESCRIPTOR_IDS = ['institution', 'product', 'variable']
 _SOME_TERM_IDS = ['ipsl', 'day']
@@ -23,15 +24,21 @@ def test_get_terms_in_data_descriptor() -> None:
 
 
 def test_find_term_in_data_descriptor() -> None:
+    settings = SearchSettings(type=SearchType.LIKE)
     for item in _SOME_TERM_REQUESTS:
         univers.find_terms_in_data_descriptor(**item)
+        univers.find_terms_in_data_descriptor(**item, settings=settings)
 
 
 def test_find_terms_in_univers() -> None:
+    settings = SearchSettings(type=SearchType.LIKE)
     for term_id in _SOME_TERM_IDS:
         univers.find_terms_in_univers(term_id)
+        univers.find_terms_in_univers(term_id, settings=settings)
 
 
 def test_find_data_descriptor_in_univers() -> None:
+    settings = SearchSettings(type=SearchType.LIKE)
     for data_descriptor_id in _SOME_DATA_DESCRIPTOR_IDS:
         univers.find_data_descriptors_in_univers(data_descriptor_id)
+        univers.find_data_descriptors_in_univers(data_descriptor_id, settings=settings)
