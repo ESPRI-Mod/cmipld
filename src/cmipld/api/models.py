8a44542a145545b6e2babee693f54d8838cace88
Sébastien Gardoll <sebastien@gardoll.fr>
2024-12-11 14:28:16 +0100

    feat: add generic models

---
+++ src/cmipld/api/models.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, ConfigDict
+
+class GenericTerm(BaseModel):
+    id: str
+    type: str
+    model_config = ConfigDict(extra = "allow")
+
+
+class GenericPlainTerm(GenericTerm):
+    drs_name: str
+    
+
+class GenericTermPatern(GenericTerm):
+    regex: str
+
+
+class CompositePart(BaseModel):
+    id: str
+    type: str
+    is_required: bool
+
+
+class GenericTermComposite(GenericTerm):
+    separator: str
+    parts: list[CompositePart]
