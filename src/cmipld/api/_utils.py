82e582ac63114aee3cbe84a55571544c1fdf2f9a
Sébastien Gardoll <sebastien@gardoll.fr>
2024-12-16 19:56:24 +0100

    refactore: move get_pydantic_class function

--- src/cmipld/api/_utils.py
+++ src/cmipld/api/_utils.py
@@ -9,10 +9,17 @@ from cmipld.core.db.models.universe import UTerm
 from cmipld.api.search import SearchSettings, SearchType
 import cmipld.core.service as service
 
-from cmipld.api.data_descriptors import get_pydantic_class
+from cmipld.api.data_descriptors import DATA_DESCRIPTOR_CLASS_MAPPING
 
 
 UNIVERSE_DB_CONNECTION = service.state_service.universe.db_connection
+
+
+def get_pydantic_class(data_descriptor_id_or_term_type: str) -> type[BaseModel]:
+    if data_descriptor_id_or_term_type in DATA_DESCRIPTOR_CLASS_MAPPING:
+        return DATA_DESCRIPTOR_CLASS_MAPPING[data_descriptor_id_or_term_type]
+    else:
+        raise ValueError(f"{data_descriptor_id_or_term_type} pydantic class not found")
 
 
 def get_universe_session() -> Session:
