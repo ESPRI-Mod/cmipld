3f78f7d07995f25521ef04b1ac8c69f269cd8827
TROUSSELLIER Laurent <laurent.troussellier@ipsl.fr>
2024-12-17 11:09:29 +0100

    chore: clean dead code

--- src/cmipld/settings.py
+++ src/cmipld/settings.py
@@ -3,13 +3,15 @@ import os
 import sys
 from pathlib import Path
 
+
 LOG_HANDLERS = [logging.StreamHandler(sys.stdout)]
 LOG_FORMAT = "[%(name)s][%(levelname)s] %(message)s"
 LOG_LEVEL = logging.INFO
 
-DIRNAME_AND_FILENAME_SEPARATOR = "_"
+logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT, handlers=LOG_HANDLERS)
 
-ROOT_DIR_PATH = Path(os.path.abspath(__file__)).parents[2]
+
+DIRNAME_AND_FILENAME_SEPARATOR = "_"
 
 PROJECT_SPECS_FILENAME = "project_specs.json"
 PROJECT_ID_JSON_KEY = "project_id"
@@ -22,15 +24,5 @@ PATTERN_JSON_KEY = 'regex'
 TERM_TYPE_JSON_KEY = 'type'
 DRS_SPECS_JSON_KEY = 'drs_name'
 SQLITE_FIRST_PK = 1
-
-logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT, handlers=LOG_HANDLERS)
-
-# TODO: file names should be rework.
-SKIPPED_FILE_DIR_NAME_PREFIXES = ['_', '.', 'src']
-
-# TODO: should be replaced by the type key in term json files.
 DATA_DESCRIPTOR_JSON_KEY = "@base"
 
-# TODO: this variable lists the data descriptors whose terms are references to terms from other data descriptors.
-# This list will be removed when mechanism that automatically recognizes such data descriptors, will be implemented.
-DATA_DESCRIPTORS_WHOSE_TERMS_ARE_REFERENCES_TO_OTHER_TERMS = ['organisation']
