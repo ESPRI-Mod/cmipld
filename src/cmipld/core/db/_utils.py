f15f6bcc52f0a851dc72669653085911cdca7aaf
Sébastien Gardoll <sebastien@gardoll.fr>
2024-12-17 10:44:54 +0100

    fix: regeneration of the dbs

--- src/cmipld/core/db/_utils.py
+++ src/cmipld/core/db/_utils.py
@@ -7,19 +7,10 @@ from sqlmodel import Session, create_engine
 def read_json_file(json_file_path: Path) -> dict:
     return json.loads(json_file_path.read_text())
 
-# Singleton for SQLModel engines.
-# Not thread safe.
 class DBConnection:
     SQLITE_URL_PREFIX = 'sqlite://'
-    _ENGINES: dict[str, Engine] = dict()
-
     def __init__(self, db_file_path: Path, echo: bool = False) -> None:
-        absolute_str_path = str(db_file_path.absolute())
-        if absolute_str_path in DBConnection._ENGINES:
-            self.engine = DBConnection._ENGINES[absolute_str_path]
-        else:
-            self.engine = create_engine(f'{DBConnection.SQLITE_URL_PREFIX}/{absolute_str_path}', echo=echo)
-            DBConnection._ENGINES[absolute_str_path] = self.engine
+        self.engine = create_engine(f'{DBConnection.SQLITE_URL_PREFIX}/{db_file_path}', echo=echo)
         self.name = db_file_path.stem
         self.file_path = db_file_path.absolute()
 
